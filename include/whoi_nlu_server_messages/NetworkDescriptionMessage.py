33bcaa6018cbef5ad5e640ed7b652b816f57aa34
Andrea F. Daniele <afdaniele@ttic.edu>
2021-09-02 16:40:46 -0400

    changed default values for int field from None to 0

--- include/whoi_nlu_server_messages/NetworkDescriptionMessage.py
+++ include/whoi_nlu_server_messages/NetworkDescriptionMessage.py
@@ -8,10 +8,10 @@ from whoi_nlu_server_messages.CBorMessage import CBorMessage
 @dataclasses.dataclass
 class NetworkDescriptionMessage(CBorMessage):
     data_hostname: Optional[str] = None
-    data_port: Optional[int] = None
+    data_port: Optional[int] = 0
     control_hostname: Optional[str] = None
-    control_port: Optional[int] = None
+    control_port: Optional[int] = 0
     speech_to_text_hostname: Optional[str] = None
-    speech_to_text_port: Optional[int] = None
+    speech_to_text_port: Optional[int] = 0
     h2sl_hostname: Optional[str] = None
-    h2sl_port: Optional[int] = None
+    h2sl_port: Optional[int] = 0
