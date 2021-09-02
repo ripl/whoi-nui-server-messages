71c73df5eb63f768d7f96dcb29d6cf53c8b84a65
Andrea F. Daniele <afdaniele@ttic.edu>
2021-09-02 16:43:55 -0400

    minor

--- include/whoi_nlu_server_messages/NetworkDescriptionMessage.py
+++ include/whoi_nlu_server_messages/NetworkDescriptionMessage.py
@@ -7,11 +7,11 @@ from whoi_nlu_server_messages.CBorMessage import CBorMessage
 
 @dataclasses.dataclass
 class NetworkDescriptionMessage(CBorMessage):
-    data_hostname: Optional[str] = None
-    data_port: Optional[int] = 0
     control_hostname: Optional[str] = None
-    control_port: Optional[int] = 0
+    control_port: int = 0
+    data_hostname: Optional[str] = None
+    data_port: int = 0
     speech_to_text_hostname: Optional[str] = None
-    speech_to_text_port: Optional[int] = 0
+    speech_to_text_port: int = 0
     h2sl_hostname: Optional[str] = None
-    h2sl_port: Optional[int] = 0
+    h2sl_port: int = 0
