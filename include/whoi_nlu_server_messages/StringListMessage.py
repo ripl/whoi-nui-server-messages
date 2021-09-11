c02b70f5d98b60eb9700486d78508ae3b95326cd
Andrea F. Daniele <afdaniele@ttic.edu>
2021-09-11 14:48:30 -0500

    mino

--- include/whoi_nlu_server_messages/StringListMessage.py
+++ include/whoi_nlu_server_messages/StringListMessage.py
@@ -1,9 +1,8 @@
 import dataclasses
-from typing import List
 
 from whoi_nlu_server_messages.CBorMessage import CBorMessage
 
 
 @dataclasses.dataclass
 class StringListMessage(CBorMessage):
-    data: List[str]
+    data: list
