c0973cbce16a79838977d774bda7159ec96d4c07
Andrea F. Daniele <afdaniele@ttic.edu>
2021-09-24 17:22:00 -0500

    added RGBDCompressedImageMessage.py

--- include/whoi_nlu_server_messages/RGBDImageMessage2.py
+++ include/whoi_nlu_server_messages/RGBDImageMessage2.py
@@ -5,12 +5,16 @@ from whoi_nlu_server_messages.HeaderMessage import HeaderMessage
 
 
 @dataclasses.dataclass
-class RGBDCompressedImageMessage(CBorMessage):
+class RGBDImageMessage2(CBorMessage):
     header: HeaderMessage
     format: str
     data: bytearray
     # Depth data uses raw image instead of compressed
+    d_height: int
+    d_width: int
     d_format: str
+    d_is_bigendian: int
+    d_step: int
     d_data: bytearray
 
     # Projection/camera matrix
